1284231726e55be1fa9f08753178bc5b1cdb2f4a
DavidO28 <damers22@yahoo.com>
2023-08-16 12:39:31 +0400

    big updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import axios from 'axios';
-import video from './assets/myvideo.mp4';
+
+import windspd from "./assets/windspd.png";
+import humidity from "./assets/humidity.png";
+import clear from "./assets/cleary.gif";
+import rain from "./assets/rainy.gif";
+import snow from "./assets/snowy.gif";
+import gif from "./assets/gifgif.gif";
 
 function App() {
   const [data, setData] = useState({});
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState("Tbilisi");
 
-
+  let myimg = gif;
+  if (data.weather) {
+    if (data.weather[0].main == "Clear") {
+      myimg = clear;
+    } else if (data.weather[0].main == "Rain") {
+      myimg = rain;
+    } else {
+      myimg = snow;
+    }
+  }
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c73986543f4485fa7a8de8ea178ba006&units=metric`;
 
   const searchLocation = (event) => {
@@ -18,21 +33,30 @@ function App() {
     }
   };
 
+  const handleClick = (e) => {
+    e.preventDefault();
+    axios.get(url).then((response) => {
+      setData(response.data)
+    })
+    setLocation("")
+  };
+
+
   return (
     <>
-      <video className='videoTag' autoPlay loop muted>
-        <source src={video} type='video/mp4' />
-      </video>
+      <div className='myimageTag' >
+        <img src={`${myimg}`} />
+      </div>
       <div className="App">
-        <div className="search">
-          <input type="text"
+        <form className="search">
+          <input className="myinput" type="text"
             value={location}
             placeholder="შეიყვანეთ ადგილი"
             onKeyDown={searchLocation}
             onChange={event => setLocation(event.target.value)}
           />
-
-        </div>
+          <button className="mybutton" type="submit" onClick={handleClick}>ძებნა</button>
+        </form>
         <div className="info">
           <div className="location">
             {data.name ? <h3>ადგილმდებარეობა {data.name}</h3> : null}
@@ -43,10 +67,10 @@ function App() {
         </div>
         <div className="more_info">
           <div className="humidity">
-            {data.main ? <h4>ტენიანობა {data.main.humidity}%</h4> : null}
+            {data.main ? <h4> <img className="humspd" src={`${humidity}`} /> ტენიანობა {data.main.humidity}%</h4> : null}
           </div>
           <div className="wind">
-            {data.main ? <h4>ქარის სიჩქარე {data.wind.speed.toFixed()} KM/H</h4> : null}
+            {data.main ? <h4><img className="humspd" src={`${windspd}`} /> ქარის სიჩქარე {data.wind.speed.toFixed()} KM/H</h4> : null}
           </div>
         </div>
       </div>
